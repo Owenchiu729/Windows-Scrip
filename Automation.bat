d411e8f3b48e44b64e481f35b83e2c28931bcdb1
Owenchiu729 <112868814+Owenchiu729@users.noreply.github.com>
2022-11-03 15:29:36 +0800

    Update and rename README.md to Automation.bat

---
+++ Automation.bat
@@ -0,0 +1,17 @@
+# Windows-Scrip
+REM ===== Start =====
+@echo off
+start http://itils.edcove.com/arsys/shared/login.jsp?/arsys/home
+start http://10.29.24.100/NOC
+start https://e-service.aceredc.com/admin/index
+start http://10.29.32.101/NmConsole/Workspace/HomeWorkspace/HomeWorkspace.asp?
+start https://km.aceredc.com/SmartKMS/LoginPage.action
+
+start  "" "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.exe" 
+start  "" "C:\Users\2007018\Desktop\202204 Extension.pdf"
+start  "" "C:\Users\2007018\Desktop\NOVE_V1.1.xlsx"
+start  "" "D:\Notepad.txt"
+start  "" "C:\Users\2007018\AppData\Local\LINE\bin\LineLauncher.exe"
+@echo.
+EXIT
+REM =====  End  =====
